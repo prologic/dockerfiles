b2f41ca87c65e7e355e90d84942a2f3d03d0464e
James Mills <prologic@shortcircuit.net.au>
2018-05-04 23:22:27 -0700

    Fixed bug in alert

--- alert/alert.sh
+++ alert/alert.sh
@@ -3,10 +3,10 @@
 IRC_NICK="${IRC_NICK:-alertmanager}"
 IRC_HOST="${IRC_HOST:-irc.mills.io}"
 IRC_PORT="${IRC_PORT:-6667}"
-IRC_CHAN="${IRC_CHAN:-alerts}"
+IRC_CHAN="${IRC_CHAN:-#alerts}"
 
 payload="$(cat - | jq -r '.payload' | base64 -d)"
 
 echo "${payload}" \
   | jq -r '.alerts[] | "ALERT: \(.labels.alertname) \(.status) - \(.annotations.summary)"' \
-  | irccat -n "${IRC_NICK}" "${IRC_HOST}:${IRC_PORT}" "#${IRC_CHAN}"
+  | irccat -n "${IRC_NICK}" "${IRC_HOST}:${IRC_PORT}" "${IRC_CHAN}"
