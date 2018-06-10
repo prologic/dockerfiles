92954f40c27016b5ab89322446486564e50c8612
James Mills <prologic@shortcircuit.net.au>
2018-06-10 10:09:10 -0700

    Add support for logging into a docker registry

--- autoupdate/start.sh
+++ autoupdate/start.sh
@@ -1,6 +1,30 @@
-#!/bin/sh
+#!/bin/bash
 
 MSGBUS_URI="${MSGBUS_URI:-https://msgbus.mills.io}"
 MSGBUS_TOPIC="${MSGBUS_TOPIC:-ci.mills.io}"
+
+REGISTRY_HOSTNAME="${REGISTRY_HOSTNAME:-r.mills.io}"
+
+function docker_login() {
+  username="${REGISTRY_USERNAME}"
+  if [ "${username}" -eq "" ]; then
+    if [ -f /run/secrets/registry_username ]; then
+      username="$(cat /run/secrets/registry_username)"
+    fi
+  fi
+
+  password="${REGISTRY_PASSWORD}"
+  if [ "${password}" -eq "" ]; then
+    if [ -f /run/secrets/registry_password ]; then
+      password="$(cat /run/secrets/registry_password)"
+    fi
+  fi
+
+  if [ "${username}" -ne "" ]; then
+    docker login -u "${username}" -p "${password}"
+  fi
+}
+
+docker_login
 
 exec msgbus -d sub "${MSGBUS_TOPIC}" /usr/local/bin/autoupdate.sh "${@}"
